bc75b8317f8b2f204823d392bc69feadf25a735f
Johnny Oshika <johnnyoshika@gmail.com>
2021-12-28 10:39:07 -0800

    Add tests cases to handler.test.ts

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -9,10 +9,56 @@ describe('handle', () => {
     jest.resetModules();
   });
 
-  test('handle GET', async () => {
-    const result = await handleRequest(new Request('/', { method: 'GET' }));
-    expect(result.status).toEqual(200);
-    const text = await result.text();
-    expect(text).toEqual('request method: GET');
+  test('redirect to www.examind.io', async () => {
+    const result = await handleRequest(
+      new Request('/blog?foo=bar', { method: 'GET' }),
+    );
+    expect(result.status).toEqual(301);
+    expect(result.headers.get('Location')).toEqual(
+      'https://www.examind.io/blog?foo=bar',
+    );
+  });
+
+  test("redirect /meet to Gonzo's calendar", async () => {
+    const result = await handleRequest(new Request('/meet', { method: 'GET' }));
+    expect(result.status).toEqual(301);
+    expect(result.headers.get('Location')).toEqual(
+      'https://meetings.hubspot.com/gonzo2',
+    );
+  });
+
+  test("redirect /MeetGonzo to Gonzo's calendar", async () => {
+    const result = await handleRequest(
+      new Request('/MeetGonzo', { method: 'GET' }),
+    );
+    expect(result.status).toEqual(301);
+    expect(result.headers.get('Location')).toEqual(
+      'https://meetings.hubspot.com/gonzo2',
+    );
+  });
+
+  test("redirect /meetbrad to Brad's calendar", async () => {
+    const result = await handleRequest(
+      new Request('/meetbrad', { method: 'GET' }),
+    );
+    expect(result.status).toEqual(301);
+    expect(result.headers.get('Location')).toEqual(
+      'https://meetings.hubspot.com/brad503',
+    );
+  });
+
+  test("redirect /meetjohnny to Johnny's calendar", async () => {
+    const result = await handleRequest(
+      new Request('/meetjohnny', { method: 'GET' }),
+    );
+    expect(result.status).toEqual(301);
+    expect(result.headers.get('Location')).toEqual(
+      'https://meetings.hubspot.com/johnny125',
+    );
+  });
+
+  test('return 405 for post request', async () => {
+    const result = await handleRequest(new Request('/foo', { method: 'POST' }));
+    expect(result.status).toEqual(405);
   });
 });
