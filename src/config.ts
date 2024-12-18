999970e241f3d65adc51438bb084bd89f7a5d60f
Johnny Oshika <johnnyoshika@gmail.com>
2024-12-17 20:25:28 -0800

    Redirect instructor-help.examind.io to https://examind.gitbook.io/v1

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ export const REDIRECTS: { [key: string]: string } = {
     'https://docs.google.com/presentation/d/1NnvhgSAffESvBrRdL_jLtY6isAFxvKtNfgZwooYGQk4/edit?usp=sharing',
   'download.examind.io':
     'https://examind.lpages.co/auburn-keynote-download/',
+  'instructor-help.examind.io': 'https://examind.gitbook.io/v1',
 };
