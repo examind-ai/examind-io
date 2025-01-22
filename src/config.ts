8295b1d5c70a5684d10a4cb8794446f7769be1f9
Johnny Oshika <johnnyoshika@gmail.com>
2025-01-22 12:16:01 -0800

    Delete download.examind.io redirect

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
 export const ROOT_REDIRECTS: { [key: string]: string } = {
   'student-help.examind.io':
     'https://docs.google.com/presentation/d/1NnvhgSAffESvBrRdL_jLtY6isAFxvKtNfgZwooYGQk4/edit?usp=sharing',
-  'download.examind.io':
-    'https://examind.lpages.co/auburn-keynote-download/',
   'instructor-help.examind.io': 'https://examind.gitbook.io/v1',
 };
 
